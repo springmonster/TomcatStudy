2077c43508b377b838548057a70df31ec6ec714e
Charles Kuang <charles.kuang@homecreditcfc.cn>
2019-10-11 14:36:38 +0800

    first commit

---
+++ StudyProject/src/Request.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Request {
+	private InputStream inputStream;
+	private String uri;
+
+	public Request(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	public void parse() {
+		StringBuffer sb = new StringBuffer();
+		int i;
+		byte[] buffer = new byte[2048];
+		try {
+			i = inputStream.read(buffer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			i = -1;
+		}
+
+		for (int j = 0; j < i; j++) {
+			sb.append((char) buffer[j]);
+		}
+		System.out.println(sb.toString());
+		uri = parseUri(sb.toString());
+	}
+
+	private String parseUri(String requestString) {
+		int index1, index2;
+		index1 = requestString.indexOf(' ');
+		if (index1 != -1) {
+			index2 = requestString.indexOf(' ', index1 + 1);
+			if (index2 > index1)
+				return requestString.substring(index1 + 1, index2);
+		}
+		return null;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+}
