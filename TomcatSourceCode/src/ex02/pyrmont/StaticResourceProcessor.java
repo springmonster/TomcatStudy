776cf8c9a5927dca10dfe6e4301e4f21b7a77e9f
Charles Kuang <charles.kuang@homecreditcfc.cn>
2019-10-16 13:56:00 +0800

    refactor

---
+++ TomcatSourceCode/src/ex02/pyrmont/StaticResourceProcessor.java
@@ -0,0 +1,15 @@
+package ex02.pyrmont;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+	public void process(Request request, Response response) {
+		try {
+			response.sendStaticResource();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
