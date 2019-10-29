e7328046922b58946690725904df5762b3a12a32
Charles Kuang <charles.kuang@homecreditcfc.cn>
2019-10-29 16:11:01 +0800

    add standardwrapper

---
+++ TomcatSourceCodeIdea/src/ex11/pyrmont/core/SimpleContextConfig.java
@@ -0,0 +1,17 @@
+package ex11.pyrmont.core;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+public class SimpleContextConfig implements LifecycleListener {
+
+    public void lifecycleEvent(LifecycleEvent event) {
+        System.out.println("------> lifecycleEvent event is " + event.getType() + " <------");
+        if (Lifecycle.START_EVENT.equals(event.getType())) {
+            Context context = (Context) event.getLifecycle();
+            context.setConfigured(true);
+        }
+    }
+}
