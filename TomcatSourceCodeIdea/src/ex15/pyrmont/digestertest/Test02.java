431d9de289f92139d89df2471ed0b69152d2ae65
Charles Kuang <charles.kuang@homecreditcfc.cn>
2019-10-30 16:34:18 +0800

    add rule

--- TomcatSourceCodeIdea/src/ex15/pyrmont/digestertest/Test02.java
+++ TomcatSourceCodeIdea/src/ex15/pyrmont/digestertest/Test02.java
@@ -10,18 +10,22 @@ public class Test02 {
 
     public static void main(String[] args) {
         String path = System.getProperty("user.dir") + File.separator + "etc";
+
         File file = new File(path, "employee2.xml");
         Digester digester = new Digester();
         // add rules
         digester.addObjectCreate("employee", "ex15.pyrmont.digestertest.Employee");
         digester.addSetProperties("employee");
+
         digester.addObjectCreate("employee/office", "ex15.pyrmont.digestertest.Office");
         digester.addSetProperties("employee/office");
         digester.addSetNext("employee/office", "addOffice");
+
         digester.addObjectCreate("employee/office/address",
                 "ex15.pyrmont.digestertest.Address");
         digester.addSetProperties("employee/office/address");
         digester.addSetNext("employee/office/address", "setAddress");
+
         try {
             Employee employee = (Employee) digester.parse(file);
             ArrayList offices = employee.getOffices();
